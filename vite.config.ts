190da563cfd4088494552274728681df81c13d98
3ein39 <hussein.hany.cs@gmail.com>
2025-07-08 18:12:21 +0300

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/motion-v-playground/',
   plugins: [
     vue(),
     vueJsx(),
